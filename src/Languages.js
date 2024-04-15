91c6584c5d8320f88bfdcdaf49cc878b46c5d158
shiqi0515 <shiqi0515@outlook.com>
2024-04-15 07:01:22 +0200

    Update translations and add Swedish language

--- src/Languages.js
+++ src/Languages.js
@@ -4,16 +4,17 @@ import englishImage from './images/English.jpg';
 import chineseImage from './images/Chinese.png';
 // import frenchImage from './images/French.png';
 // import germanImage from './images/German.png';
-// import swedishImage from './images/Swedish.png';
+import swedishImage from './images/Swedish.png';
 import { useLanguage } from './LanguageProvider';
 import Header from './Header';
 
 const languages = [
     { id: 0, name: 'English', image: englishImage, code: 'en' },
     { id: 1, name: 'Chinese', image: chineseImage, code: 'zh' },
-    // { id: 2, name: 'French', image: frenchImage, code: 'fr' },
-    // { id: 3, name: 'German', image: germanImage, code: 'de' },
-    // { id: 4, name: 'Swedish', image: swedishImage, code: 'sv' },
+    { id: 2, name: 'Swedish', image: swedishImage, code: 'sv' },
+    // { id: 3, name: 'French', image: frenchImage, code: 'fr' },
+    // { id: 4, name: 'German', image: germanImage, code: 'de' },
+
 ];
 
 const LanguageCard = ({ language }) => {
